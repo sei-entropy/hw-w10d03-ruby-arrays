fd00de724e2cdcdf6507bbd28ed3a263b6878154
Sara-Kuddah <sara-kuddah@hotmail.com>
2020-03-24 18:21:53 +0300

    finish the homework

--- hw.rb
+++ hw.rb
@@ -1,73 +1,73 @@
-# # 1. Return an array of each Student's full name, upper-cased
-# students = [
-#   {
-#       first_name: 'Renad',
-#       last_name: 'Alobaid'
-#   },
-#   {
-#       first_name: 'Roba',
-#       last_name: 'Arishi',
-#   },
-#   {
-#       first_name: 'Lama',
-#       last_name: 'Alaskar',
-#   }
-# ]
+# 1. Return an array of each Student's full name, upper-cased
+students = [
+  {
+      first_name: 'Renad',
+      last_name: 'Alobaid'
+  },
+  {
+      first_name: 'Roba',
+      last_name: 'Arishi',
+  },
+  {
+      first_name: 'Lama',
+      last_name: 'Alaskar',
+  }
+]
 
-# upper_case_full_names = []
+upper_case_full_names = []
 
-# upper_case_full_names= students.map{|student| 
-# p student[:first_name].upcase+" "+student[:last_name].upcase
-# student[:first_name].upcase+" "+student[:last_name].upcase}
+upper_case_full_names= students.map{|student| 
+p student[:first_name].upcase+" "+student[:last_name].upcase
+student[:first_name].upcase+" "+student[:last_name].upcase}
 
-# p upper_case_full_names
+p upper_case_full_names
 
 
-# # 2. Find the first order for each user
-# users = [
-#   {
-#       name: 'Faisal',
-#       orders: [
-#           {
-#               description: 'a bike'
-#           }
-#       ]
-#   },
-#   {
-#       name: 'Huda',
-#       orders: [
-#           {
-#               description: 'bees'
-#           },
-#           {
-#               description: 'fishing rod'
-#           }
-#       ]
-#   },
-#   {
-#       name: 'Omar',
-#       orders: [
-#           {
-#               description: 'a MacBook'
-#           },
-#           {
-#               description: 'The West Wing DVDs'
-#           },
-#           {
-#               description: 'headphones'
-#           },
-#           {
-#               description: 'a kitten'
-#           }
-#       ]
-#   }
-# ]
+# 2. Find the first order for each user
+users = [
+  {
+      name: 'Faisal',
+      orders: [
+          {
+              description: 'a bike'
+          }
+      ]
+  },
+  {
+      name: 'Huda',
+      orders: [
+          {
+              description: 'bees'
+          },
+          {
+              description: 'fishing rod'
+          }
+      ]
+  },
+  {
+      name: 'Omar',
+      orders: [
+          {
+              description: 'a MacBook'
+          },
+          {
+              description: 'The West Wing DVDs'
+          },
+          {
+              description: 'headphones'
+          },
+          {
+              description: 'a kitten'
+          }
+      ]
+  }
+]
 
-# first_order_for_each_user = users.map {|user| 
-# p user[:orders][0]
-# user[:orders][0]}
+first_order_for_each_user = users.map {|user| 
+p user[:orders][0]
+user[:orders][0]}
 
-# p first_order_for_each_user
+p first_order_for_each_user
 
 
 # 3. Find the average amount spent on coffee, per transaction, for each person
@@ -142,3 +142,64 @@ coffee_amount: all_amount/num
 }
 
 p coffee_average_per_person
+
+
+# 4. Find the most expensive product for each store, with the store name:
+stores = [
+  {
+      store_name: 'Jarir',
+      products: [
+          {
+              description: 'Titanium',
+              price: 9384.33
+          },
+          {
+              description: 'Gold',
+              price: 345.54
+          }
+      ]
+  },
+  {
+      store_name: 'Tamimi',
+      products: [
+          {
+              description: 'Silver',
+              price: 654.44
+          },
+          {
+              description: 'Ruby',
+              price: 323.43
+          }
+      ]
+  },
+  {
+      store_name: 'Souq',
+      products: [
+          {
+              description: 'Opal',
+              price: 345.43
+          },
+          {
+              description: 'Sapphire',
+              price: 899.33
+          }
+      ]
+  }
+]
+
+most_expensive_products_by_store = stores.map{|store|
+most_expensive=0
+description=""
+store[:products].each{|type| 
+if type[:price] > most_expensive
+    most_expensive = type[:price]
+    description=type[:description]
+end   
+}
+
+{ store_name: store[:store_name],
+ most_expensive_product:{description:description, price: most_expensive}
+}
+}
+
+p most_expensive_products_by_store
