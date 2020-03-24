b483f296df6e81173e267f962d9d61adbe670537
Ralabdulwa98 <rmalabdulwahab@gmail.com>
2020-03-24 20:22:36 +0300

    Find the first order for each user

--- app.rb
+++ app.rb
@@ -24,3 +24,62 @@ upper_case_full_names = students.map do |name|
 #   p upper_case_full_names
 
 ## Answer: RENAD ALOBAID, ROBA ARISHI, LAMA ALASKAR
+
+
+##---------------------------------------------------
+
+##  2. Find the first order for each user
+
+users = [
+  {
+      name: 'Faisal',
+      orders: [
+          {
+              description: 'a bike'
+          }
+      ]
+  },
+  {
+      name: 'Huda',
+      orders: [
+          {
+              description: 'bees'
+          },
+          {
+              description: 'fishing rod'
+          }
+      ]
+  },
+  {
+      name: 'Omar',
+      orders: [
+          {
+              description: 'a MacBook'
+          },
+          {
+              description: 'The West Wing DVDs'
+          },
+          {
+              description: 'headphones'
+          },
+          {
+              description: 'a kitten'
+          }
+      ]
+  }
+]
+
+first_order_for_each_user = []
+
+first_order_for_each_user = users.map do |num|
+     "#{num[:orders].first}"
+end
+
+# p first_order_for_each_user
+
+
+## Answer: 
+# {:description=>"a bike"}
+# {:description=>"bees"}
+# {:description=>"a MacBook"}
+
